be89af4cad98c7025a1ffcf946288cb9dda8debb
Edgar <edgar.gonzalezpena@aviva.com>
2019-09-24 22:54:10 +0100

    Initial Commit

---
+++ PlayGround/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayGround
+//
+//  Created by Edgar Gonzalez Pena on 24/09/2019.
+//  Copyright © 2019 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
