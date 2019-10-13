40488b6e913a8811ed1147e41f98a05c5bbc9806
Edgar <edgar.gonzalezpena@aviva.com>
2019-10-13 22:37:29 +0100

    Add Child VC programatically

--- PlayGround/Views/CustomViewController.swift
+++ PlayGround/Views/CustomViewController.swift
@@ -15,18 +15,43 @@ class CustomViewController: UIViewController {
     fileprivate lazy var elementAction: UIView = {
         let elementAction = ElementAction.instantiateFromNib();
         elementAction?.translatesAutoresizingMaskIntoConstraints = false
+        elementAction?.backgroundColor = .orange
         return elementAction!
     }()
+    
+    fileprivate lazy var headerVC : HeaderViewController = {
+        let header = HeaderViewController()
+        header.view.backgroundColor = .green
+        header.didMove(toParent: self)
+        header.view.translatesAutoresizingMaskIntoConstraints = false
+        return header
+    }()
 
-    override func viewDidLoad() {
-        
-        view.addSubview(elementAction)
-        
+    fileprivate func setUpConstraintsForElementAction() {
         NSLayoutConstraint.activate([
             elementAction.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             elementAction.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             elementAction.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             elementAction.heightAnchor.constraint(equalToConstant: 100)
         ])
+    }
+    
+    fileprivate func setUpConstraintsForHeaderVC() {
+        NSLayoutConstraint.activate([
+            headerVC.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            headerVC.view.topAnchor.constraint(equalTo: elementAction.bottomAnchor, constant: 20),
+            headerVC.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            headerVC.view.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+    
+    override func viewDidLoad() {
+        
+        view.addSubview(elementAction)
+        addChild(headerVC)
+        view.addSubview(headerVC.view)
+        
+        setUpConstraintsForElementAction()
+        setUpConstraintsForHeaderVC()
     }
 }
---
+++ PlayGround/Views/HeaderViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Header\.swift
+//  PlayGround
+//
+//  Created by Edgar Gonzalez Pena on 13/10/2019.
+//  Copyright © 2019 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HeaderViewController: ViewController {
+    
+    fileprivate lazy var header: UIView = {
+        let header = UIView()
+        header.translatesAutoresizingMaskIntoConstraints = false
+        return header
+    }()
+    
+    override func viewDidLoad() {
+        view.addSubview(header)
+        setUpContraintsForHeader()
+    }
+    
+    func setUpContraintsForHeader() {
+        NSLayoutConstraint.activate([
+            header.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            header.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            header.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            header.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
