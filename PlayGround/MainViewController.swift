76c717899233be995c3bbf942d67b9acf8ce7f2b
Edgar <edgar.gonzalezpena@aviva.com>
2019-10-20 15:44:37 +0100

    Add Welcome and Login ViewController to learn iOS delegate protocol

--- PlayGround/MainViewController.swift
+++ PlayGround/MainViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import LBTATools
 
 class MainViewController: UIViewController {
         
@@ -6,20 +7,46 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var customControls: UIView!
     
+    @IBOutlet weak var containerView: UIView!
+    
+    fileprivate lazy var loginExample: UIView = {
+        let login = UIView()
+        login.backgroundColor = .systemBlue
+        login.translatesAutoresizingMaskIntoConstraints = false
+        return login
+    }()
+    
+    fileprivate lazy var loginExampleText: UILabel = {
+        let loginText = UILabel()
+        loginText.translatesAutoresizingMaskIntoConstraints = false
+        loginText.text = "Login Sample"
+        loginText.textColor = .white
+        return loginText
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationItem.title = "Dashboard"
         
-        basicControls.applyCustomStyle()
-        customControls.applyCustomStyle()
-        
+        containerView.addSubview(loginExample)
+        loginExample.addSubview(loginExampleText)
+
         let basicGesture = UITapGestureRecognizer(target: self, action:  #selector(self.navigateToBasic))
         let customGesture = UITapGestureRecognizer(target: self, action: #selector(self.navigateToCustom))
-        
+        let loginGesture = UITapGestureRecognizer(target: self, action: #selector(self.navigateToLogin))
+               
         basicControls.addGestureRecognizer(basicGesture)
         customControls.addGestureRecognizer(customGesture)
+        loginExample.addGestureRecognizer(loginGesture)
+                
+        basicControls.applyCustomStyle()
+        customControls.applyCustomStyle()
+        loginExample.applyCustomStyle()
+        
+        setUpContraintsForLoginSample()
+        
     }
     
     @objc func navigateToBasic(sender : UITapGestureRecognizer) {
@@ -31,5 +58,20 @@ class MainViewController: UIViewController {
         let newViewController = storyboard?.instantiateViewController(withIdentifier: String(describing: CustomViewController.self)) as! CustomViewController
         navigationController?.pushViewController(newViewController, animated: true)
         }
+    
+    @objc func navigateToLogin(sender: UITapGestureRecognizer){
+        let newViewController = storyboard?.instantiateViewController(withIdentifier: String(describing: WelcomeViewController.self)) as! WelcomeViewController
+        navigationController?.pushViewController(newViewController, animated: true)
+    }
+    
+    func setUpContraintsForLoginSample() {
+        NSLayoutConstraint.activate([
+            loginExample.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            loginExample.topAnchor.constraint(equalTo: customControls.bottomAnchor, constant: 16),
+            loginExample.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            loginExample.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        loginExampleText.centerInSuperview()
+    }
 
 }
---
+++ PlayGround/Views/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  PlayGround
+//
+//  Created by Edgar Gonzalez Pena on 20/10/2019.
+//  Copyright © 2019 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var welconeDelegate: WelcomeDelegate!
+    
+    override func viewDidLoad() {
+        navigationItem.title = "Login"
+        
+        loginButton.applyCustomStyle()
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        welconeDelegate?.didUserLogin(name: nameTextField.text ?? "", email: emailTextField.text ?? "")
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+protocol WelcomeDelegate {
+    func didUserLogin(name: String, email: String)
+}
